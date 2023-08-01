601b9d806cc0ee531e6d772fe61044c7b37d56ae
Aphelele2 <aphelelejoyi@gmail.com>
2023-08-01 11:18:02 +0200

    done

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-setTimeout(()=> {
-    console.log('5 seconds have passed')
-}, 5000)
-console.log('start');
-
-// setTimeout(function (){
-//     console.log('5 seconds have passed')
-// }, 5000)
-// console.log('start');
 
 
+// setTimeout(function () {
+//     console.log("5 seconds"); 
+// }, 5000); 
+// console.log("now");
 
+setTimeout(() => {
+    console.log('2 seconds have passed');
+}, 2000)
+console.log('start')
 
